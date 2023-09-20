1ab7bc5fe0c1c2c5e7a89ae32ea08b733820f956
daniel-majcher <danielmajcher2025@u.northwestern.edu>
2023-09-20 10:32:48 -0500

    pricing util implementation and corresponding test

--- src/pricingutil.cpp
+++ src/pricingutil.cpp
@@ -1,1 +1,17 @@
 #include "pricingutil.h"
+
+PricingUtil::PricingUtil()
+{
+    val = 0;
+}
+
+float PricingUtil::calcVal(float prevPrice, float interest, float oleoConstant)
+{
+    val = (prevPrice * (0.9 + interest)) * oleoConstant;
+    return val;
+}
+
+float PricingUtil::getVal()
+{
+    return val;
+}
