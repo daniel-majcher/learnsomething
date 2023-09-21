91141f0ecf1ac56f2fc558f86dd21a00cb89a45b
daniel-majcher <danielmajcher2025@u.northwestern.edu>
2023-09-21 10:11:39 -0500

    initial action implementation

--- src/action.cpp
+++ src/action.cpp
@@ -4,4 +4,34 @@
 
 #include "action.h"
 
-Action(bool buyInput) : buy(buyInput) {};
+
+Action::Action(bool initHold = true) : holding(initHold) {
+    initialBalance = 0; //not sure what purpose of initialBalance is
+    balance = 0;
+}
+
+bool Action::buy(double price) {
+    if (!holding && balance>=price) {
+        balance -= price;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool Action::sell(double price) {
+    if (holding) {
+        balance += price;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool Action::getHold() {
+    return holding;
+}
+
+double Action::getBalance() {
+    return balance;
+}
