23fb067ca6a38a903b4b01ba371b4d034e85f094
daniel-majcher <danielmajcher2025@u.northwestern.edu>
2023-09-23 21:22:15 -0500

    wip. basic sell checks

--- src/hackathonbot.cpp
+++ src/hackathonbot.cpp
@@ -3,3 +3,60 @@
 //
 
 #include "hackathonbot.h"
+
+HackathonBot::HackathonBot() {
+    actionPrice = 100;
+    prev3Prices = {100, 100, 100}; //new price at back. Old prices in front ({three, two, one})
+    upWindows = 0;   
+    downWindows = 0;
+}
+
+bool HackathonBot::isHolding() {
+    return getHold();
+}
+
+void HackathonBot::UpdateState(float price) {
+    if (price > prev3Prices.back()) {
+        upWindows++;
+        downWindows=0;
+    } else if (price < prev3Prices.back()) {
+        downWindows++;
+        upWindows=0;
+    } else {
+        upWindows=0;
+        downWindows=0;
+    }
+    
+}
+
+bool HackathonBot::CheckSell(float price) {
+    return ( price > (1.89)*actionPrice //89% increase
+        || upWindows == 52
+        || downWindows == 47
+        || price < (.38)*actionPrice); //62% drop
+    //wip
+}
+
+bool HackathonBot::CheckBuy(float price) {
+    return true; //wip
+}
+
+void HackathonBot::takeAction(float price) {
+    UpdateState(price);
+    if (getHold()) {
+        //check if you should sell
+        if (CheckSell(price)) {
+            sell(price);
+            return;
+        }
+    } else {
+        if (CheckBuy(price)) {
+            buy(price);
+            return;
+        }
+    }
+
+    prev3Prices.erase(prev3Prices.begin()); //update after you're done using it
+    prev3Prices.push_back(price);
+}
+
--- src/hackathonbot.h
+++ src/hackathonbot.h
@@ -8,15 +8,31 @@
 #ifndef LEARNSOMETHING_HACKATHONBOT_H
 #define LEARNSOMETHING_HACKATHONBOT_H
 
-class HackathonBot {
+//inherit from action for basic action handling
+class HackathonBot : Action {
 public:
     HackathonBot();
     void takeAction(float price);
-    double getBalance();
     bool isHolding();
 private:
-    double balance;
-    bool holding;
+    double actionPrice; //sell price you sold it at or buy price you bought it at
+    int upWindows;
+    int downWindows;
+    std::vector<double> prev3Prices;
+    void UpdateState(float price);
+    // Method to buy a specific quantity at a specific price
+    bool CheckSell(float price);
+    bool CheckBuy(float price);
+
+    bool sell(double price) {
+        Action::sell(price);
+        actionPrice = price;
+    }
+
+    bool buy(double price) {
+        Action::buy(price);
+        actionPrice = price;
+    }
 };
 
 #endif //LEARNSOMETHING_HACKATHONBOT_H
